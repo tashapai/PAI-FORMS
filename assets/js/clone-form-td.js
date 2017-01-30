f1077636df05ac81b6d001b30f9ee8ff8507a89c
Tasha <teaton@paifashion.com>
2017-01-30 15:00:53 -0600

    asiartpack.html and embroidery.html tweaking
    Small edits to asiartpack.html
    Added more fields & functionality to dynamic embroidery.html

--- assets/js/clone-form-td.js
+++ assets/js/clone-form-td.js
@@ -28,10 +28,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 $(function () {
-    $('#btnAdd').click(function () {
-        var num     = $('.clonedInput').length, // Checks to see how many "duplicatable" input fields we currently have
+    $('#btnAddTape').click(function () {
+        var num     = $('.clonedInput1').length, // Checks to see how many "duplicatable" input fields we currently have
             newNum  = new Number(num + 1),      // The numeric ID of the new input field being added, increasing by 1 each time
-            newElem = $('#entry' + num).clone().attr('id', 'entry' + newNum).fadeIn('slow'); // create the new element via clone(), and manipulate it's ID using newNum value
+            newElem = $('#tape' + num).clone().attr('id', 'tape' + newNum).fadeIn('slow'); // create the new element via clone(), and manipulate it's ID using newNum value
     
     /*  This is where we manipulate the name/id values of the input inside the new, cloned element
         Below are examples of what forms elements you can clone, but not the only ones.
@@ -40,37 +40,86 @@ $(function () {
         Keep in mind that the .val() method is what clears the element when it gets cloned. Radio and checkboxes need .val([]) instead of .val('').
     */
         // H2 - section
-        newElem.find('.heading-reference').attr('id', 'ID' + newNum + '_reference').attr('name', 'ID' + newNum + '_reference').html('Entry #' + newNum);
+        newElem.find('.heading-reference').attr('id', 'ID' + newNum + '_reference').attr('name', 'ID' + newNum + '_reference').html('Tape Entry #' + newNum);
 
     // Insert the new element after the last "duplicatable" input field
-        $('#entry' + num).after(newElem);
+        $('#tape' + num).after(newElem);
         $('#ID' + newNum + '_title').focus();
 
     // Enable the "remove" button. This only shows once you have a duplicated section.
-        $('#btnDel').attr('disabled', false);
+        $('#btnDelTape').attr('disabled', false);
 
-    // Right now you can only add 4 sections, for a total of 5. Change '5' below to the max number of sections you want to allow.
-        if (newNum == 5)
-        $('#btnAdd').attr('disabled', true).prop('value', "You've reached the limit"); // value here updates the text in the 'add' button when the limit is reached 
+    // Right now you can only add 3 sections, for a total of 4. Change '4' below to the max number of sections you want to allow.
+        if (newNum == 4)
+        $('#btnAddTape').attr('disabled', true).prop('value', "You've reached the limit"); // value here updates the text in the 'add' button when the limit is reached 
     });
 
-    $('#btnDel').click(function () {
+    $('#btnDelTape').click(function () {
     // Confirmation dialog box. Works on all desktop browsers and iPhone.
         if (confirm("Are you sure you wish to remove this section? This cannot be undone."))
             {
-                var num = $('.clonedInput').length;
+                var num = $('.clonedInput1').length;
                 // how many "duplicatable" input fields we currently have
-                $('#entry' + num).slideUp('slow', function () {$(this).remove();
+                $('#tape' + num).slideUp('slow', function () {$(this).remove();
                 // if only one element remains, disable the "remove" button
                     if (num -1 === 1)
-                $('#btnDel').attr('disabled', true);
+                $('#btnDelTape').attr('disabled', true);
                 // enable the "add" button
-                $('#btnAdd').attr('disabled', false).prop('value', "add section");});
+                $('#btnAddTape').attr('disabled', false).prop('value', "add section");});
             }
         return false; // Removes the last section you added
     });
     // Enable the "add" button
-    $('#btnAdd').attr('disabled', false);
+    $('#btnAddTape').attr('disabled', false);
     // Disable the "remove" button
-    $('#btnDel').attr('disabled', true);
+    $('#btnDelTape').attr('disabled', true);
 });
+
+$(function () {
+    $('#btnAddSample').click(function () {
+        var num     = $('.clonedInput2').length, // Checks to see how many "duplicatable" input fields we currently have
+            newNum  = new Number(num + 1),      // The numeric ID of the new input field being added, increasing by 1 each time
+            newElem = $('#sample' + num).clone().attr('id', 'sample' + newNum).fadeIn('slow'); // create the new element via clone(), and manipulate it's ID using newNum value
+    
+    /*  This is where we manipulate the name/id values of the input inside the new, cloned element
+        Below are examples of what forms elements you can clone, but not the only ones.
+        There are 2 basic structures below: one for an H2, and one for form elements.
+        To make more, you can copy the one for form elements and simply update the classes for its label and input.
+        Keep in mind that the .val() method is what clears the element when it gets cloned. Radio and checkboxes need .val([]) instead of .val('').
+    */
+        // H2 - section
+        newElem.find('.heading-reference').attr('id', 'ID' + newNum + '_reference').attr('name', 'ID' + newNum + '_reference').html('Sample Entry #' + newNum);
+
+    // Insert the new element after the last "duplicatable" input field
+        $('#sample' + num).after(newElem);
+        $('#ID' + newNum + '_title').focus();
+
+    // Enable the "remove" button. This only shows once you have a duplicated section.
+        $('#btnDelSample').attr('disabled', false);
+
+    // Right now you can only add 5 sections, for a total of 6. Change '6' below to the max number of sections you want to allow.
+        if (newNum == 6)
+        $('#btnAddSample').attr('disabled', true).prop('value', "You've reached the limit"); // value here updates the text in the 'add' button when the limit is reached 
+    });
+
+    $('#btnDelSample').click(function () {
+    // Confirmation dialog box. Works on all desktop browsers and iPhone.
+        if (confirm("Are you sure you wish to remove this section? This cannot be undone."))
+            {
+                var num = $('.clonedInput2').length;
+                // how many "duplicatable" input fields we currently have
+                $('#sample' + num).slideUp('slow', function () {$(this).remove();
+                // if only one element remains, disable the "remove" button
+                    if (num -1 === 1)
+                $('#btnDelSample').attr('disabled', true);
+                // enable the "add" button
+                $('#btnAddSample').attr('disabled', false).prop('value', "add section");});
+            }
+        return false; // Removes the last section you added
+    });
+    // Enable the "add" button
+    $('#btnAddSample').attr('disabled', false);
+    // Disable the "remove" button
+    $('#btnDelSample').attr('disabled', true);
+});
+
