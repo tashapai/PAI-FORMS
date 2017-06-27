f7e8bd4eac15e4d397ce86ae0bcbb5f1dcbe01ec
Tasha <teaton@paifashion.com>
2017-06-27 16:29:14 -0500

    Edits to Imperial CSRs

---
+++ assets/js/csr-imperial.js
@@ -0,0 +1,105 @@
+$('#csr_name').change(function() {
+	var myValue = $(this).val();
+	switch (myValue) {
+	
+	case 'Ashley Tucker':
+		$('input[name="csr_email"]').val('atucker@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x390');
+		break;	
+				
+	case 'Chad Saladin':
+		$('input[name="csr_email"]').val('csaladin@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x342');
+		break;	
+		
+	case 'Chelsey Hanneken':
+		$('input[name="csr_email"]').val('channeken@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x382');
+		break;	
+		
+	case 'Chris Frame':
+		$('input[name="csr_email"]').val('cframe@paifashion.com');
+		$('input[name="csr_phone"]').val('800-950-1916 x370');
+		break;	
+				
+	case 'Emily Mangles':
+		$('input[name="csr_email"]').val('emangles@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x341');
+		break;	
+			
+	case 'Gina Hayes':
+		$('input[name="csr_email"]').val('ghayes@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x391');
+		break;	
+		
+	case 'Joe Pendergrass':
+		$('input[name="csr_email"]').val('jpendergrass@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x381');
+		break;
+		
+	case 'John Meusch':
+		$('input[name="csr_email"]').val('jmeusch@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x340');
+		break;
+		
+	case 'John Gaster':
+		$('input[name="csr_email"]').val('jgaster@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x344');
+		break;
+		
+	case 'Kara Taylor':
+		$('input[name="csr_email"]').val('ktaylor@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x501');
+		break;
+		
+	case 'Kelly Hohe':
+		$('input[name="csr_email"]').val('khohe@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x392');
+		break;
+		
+	case 'Kim Latham':
+		$('input[name="csr_email"]').val('kiml@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x337');
+		break;
+		
+	case 'Michelle Fountain':
+		$('input[name="csr_email"]').val('mfountain@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x335');
+		break;
+		
+	case 'Russ Meyer':
+		$('input[name="csr_email"]').val('rmeyer@paifashion.com');
+		$('input[name="csr_phone"]').val('800-950-1916 x455');
+		break;
+	
+	case 'Shelley Brown':
+		$('input[name="csr_email"]').val('sbrown@paifashion.com');
+		$('input[name="csr_phone"]').val('800-950-1916 x380');
+		break;
+
+	case 'Shelly Pigg':
+		$('input[name="csr_email"]').val('spigg@paifashion.com');
+		$('input[name="csr_phone"]').val('800-950-1916 x340');
+		break;
+
+	case 'Susan Tucker':
+		$('input[name="csr_email"]').val('stucker@paifashion.com');
+		$('input[name="csr_phone"]').val('800-950-1916 x2003');
+		break;
+
+	case 'Terri Thater':
+		$('input[name="csr_email"]').val('tthater@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x343');
+		break;
+
+	case 'Trish Brewer':
+		$('input[name="csr_email"]').val('tbrewer@paifashion.com');
+		$('input[name="csr_phone"]').val('800-757-8545');
+		break;
+	
+	case 'Unassigned':
+		$('input[name="csr_email"]').val('mscott@paifashion.com');
+		$('input[name="csr_phone"]').val('573-732-4411 x133');
+		break;										
+	}
+});
